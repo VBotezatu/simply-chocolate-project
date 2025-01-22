c6722af8d9980535cee44b68ffdeab32dbfda339
VBotezatu <148051501+VBotezatu@users.noreply.github.com>
2025-01-22 14:35:06 +0100

    ffxo

---
+++ styles/js/modal-buy.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-buy-open]"),
+      closeModalBtn: document.querySelector("[data-buy-close]"),
+      modal: document.querySelector("[data-buy]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
